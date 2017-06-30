3217e791c7829856e7b714101610e7b21a90b6e6
pero5ar <pero.5ar@gmail.com>
2017-06-30 03:49:16 +0200

    improve filter, add category tabs

--- react-app/src/components/Main.jsx
+++ react-app/src/components/Main.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-bootstrap/lib';
+import { ITEM_TYPES } from '../modules/Constants';
 import ItemThumbnailContainer from './item/ItemThumbnailContainer';
 import LoadingStatus from './shared/LoadingStatus';
 
@@ -6,12 +8,19 @@ class Main extends Component {
     constructor(props) {
         super(props);
 
+        this.handleSelectCategory = this.handleSelectCategory.bind(this);
+
         this.state = {
             items: [],
             isLoading: false,
             isError: false,
+            categoryId: -1,
             filterText: this.props.location.search ? this.props.location.search.substr(1) : ""
         };
+    }
+
+    handleSelectCategory(key) {
+        this.setState({ categoryId: key-2 })
     }
 
     componentDidMount() {
@@ -39,13 +48,26 @@ class Main extends Component {
 
     getFilteredItems() {
         let filter = this.state.filterText.toLowerCase();
-        return this.state.items.filter(item => item.name && item.name.toLowerCase().startsWith(filter));
+        return this.state.items.filter(item => ( this.state.categoryId === -1
+                                                    || item.type === ITEM_TYPES[this.state.categoryId] )
+                                            && (( item.name
+                                                    && item.name.toLowerCase().includes(filter) )
+                                                || ( item.description
+                                                    && item.description.substr(0, 200).toLowerCase().includes(filter) ))
+                                            );
     }
 
     render() {
+        let tabs = ITEM_TYPES.map((type, index) => (
+            <Tab eventKey={index+2} title={type.toUpperCase()} />
+        ));
+        tabs.unshift(<Tab eventKey={1} title="SVE" />);
+
         return (
             <div>
-                { this.state.filterText }
+                <Tabs defaultActiveKey={1} animation={false} onSelect={this.handleSelectCategory}>
+                    {tabs}
+                </Tabs>
                 <LoadingStatus isError={this.state.isError} isLoading={this.state.isLoading} />
                 <ItemThumbnailContainer items={ this.getFilteredItems() } />
             </div>
