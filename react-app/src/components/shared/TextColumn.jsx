1aaf01dcc7c0adda011c6178c463ce5f307c8f29
pero5ar <pero.5ar@gmail.com>
2017-07-01 20:56:14 +0200

    remove extra line

--- react-app/src/components/shared/TextColumn.jsx
+++ react-app/src/components/shared/TextColumn.jsx
@@ -3,8 +3,6 @@ import {Col} from 'react-bootstrap/lib';
 
 class TextColumn extends Component {
     render() {
-        let bodyText = this.props.text.body.replace("\n", "<br />");
-
         return (
             <Col sm={this.props.size.sm} md={this.props.size.md}>
                 <h2>{this.props.text.title}</h2>
